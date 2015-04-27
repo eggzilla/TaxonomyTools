460354c91ab89df63e281915fffb5e7b0144e9c2
Florian Eggenhofer <egg@tbi.univie.ac.at>
2015-04-27 18:09:54 +0200

    Added reading of nodes dmp file and applied some functions from Bio.Taxonomy

--- src/Bio/TaxonomyTools.hs
+++ src/Bio/TaxonomyTools.hs
@@ -7,20 +7,35 @@ module Main where
 import Prelude 
 import System.Console.CmdArgs    
 import Bio.Taxonomy
+import Data.Either.Unwrap
+import Data.Graph.Inductive
 
 --------------------------------------------------------
 
 data Options = Options            
-  { inputFilePath :: String,
+  { taxNodesFilePath :: String,
     outputPath :: String
   } deriving (Show,Data,Typeable)
 
+options :: Options
 options = Options
-  { inputFilePath = def &= name "i" &= help "Path to input fasta file",
+  { taxNodesFilePath = def &= name "i" &= help "Path to input NCBI taxonomy dump nodes file",
     outputPath = def &= name "o" &= help "Path to output directory"
   } &= summary "TaxonomyTools" &= help "Florian Eggenhofer - 2015" &= verbosity   
 
 main :: IO ()
 main = do
   Options{..} <- cmdArgs options
-  print inputFilePath 
+  graphOutput <- readTaxonomy taxNodesFilePath
+  if (isLeft graphOutput)
+     then do
+       print ("Could not parse provided tax nodes file" ++ (fromLeft graphOutput))
+     else do 
+       let graph = fromRight graphOutput
+       let rootNode = bfs (1 :: Node) graph
+       print rootNode
+       let colirootpath = sp (561 :: Node) (1 :: Node) graph
+       print colirootpath
+       let colilabel = lab graph (561 :: Node)
+       print colilabel
+       print taxNodesFilePath
