1bf927380ccbc4925e95257bbc8616b913a59629
Florian Eggenhofer <egg@tbi.univie.ac.at>
2015-05-04 11:50:26 +0200

    Added reading of input taxidcsv and treecomparsion and drawing functions to Ids2TreeCompare

--- src/Bio/Ids2TreeCompare.hs
+++ src/Bio/Ids2TreeCompare.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE DeriveDataTypeable #-}
 -- | Ids2TreeCompare
--- dist/build/TaxonomyTools/TaxonomyTools -i /scratch/egg/data/taxdump/ -o /home/mescalin/egg/current/Projects/Haskell/TaxonomyTools/
--- dot -Tsvg taxonomy.dot -o taxonomy.svg
+-- dist/build/TaxonomyTools/Ids2TreeCompare -i /scratch/egg/data/taxdump/ -t /home/mescalin/egg/current/Projects/Haskell/TaxonomyTools/input.csv -o /home/mescalin/egg/current/Projects/Haskell/TaxonomyTools/
+-- dot -Tsvg comparison.dot -o comparison.svg
 module Main where
 
 import Prelude 
@@ -11,6 +11,7 @@ import Bio.Taxonomy
 import Data.Either.Unwrap
 import Data.Graph.Inductive
 import Data.Csv
+import Data.List
 import qualified Data.Vector as V
 import qualified Data.ByteString.Lazy.Char8 as L
 import Data.Char
@@ -18,16 +19,14 @@ import Data.Char
 
 data Options = Options            
   { taxDumpDirectoryPath :: String,
-    taxNodeList1FilePath :: String,
-    taxNodeList2FilePath :: String,  
+    taxNodeCSVFilePath :: String,
     outputDirectoryPath :: String
   } deriving (Show,Data,Typeable)
 
 options :: Options
 options = Options
   { taxDumpDirectoryPath = def &= name "i" &= help "Path to input NCBI taxonomy dump files directory",
-    taxNodeList1FilePath = def &= name "t" &= help "Path to input taxonomy id list 1",
-    taxNodeList2FilePath = def &= name "t" &= help "Path to input taxonomy id list 2",
+    taxNodeCSVFilePath = def &= name "t" &= help "Path to input taxonomy csv, each column with comma separated taxids represents one tree",
     outputDirectoryPath = def &= name "o" &= help "Path to output directory"
   } &= summary "Ids2TreeCompare" &= help "Florian Eggenhofer - 2015" &= verbosity   
 
@@ -39,9 +38,22 @@ main = do
      then do
        print ("Could not parse provided taxonomy dump files" ++ show (fromLeft graphOutput))
      else do 
-       taxidtable1 <- readFile taxNodeListFilePath1
-       taxidtable2 <- readFile taxNodeListFilePath2
-       let taxidtableentries1 = map (\l -> read l :: Int) (drop 1 (lines taxidtable1))
-       let taxidtableentries2 = map (\l -> read l :: Int) (drop 1 (lines taxidtable2))
-       print taxidtableentries1
-       print taxidtableentries2
+       let graph = fromRight graphOutput
+       treesTaxids <- extractTreesTaxidsCSV taxNodeCSVFilePath
+       let subgraphs  = map (\treeTaxids -> extractTaxonomySubTreebyLevel treeTaxids graph (Just 3)) treesTaxids
+       let comparisonGraph = compareSubTrees subgraphs
+       print comparisonGraph
+       let treeComparison = drawTreeComparison comparisonGraph
+       print treeComparison
+      
+-- | Extract taxids from RNAlien result.csv 
+extractTreesTaxidsCSV :: String -> IO [[Node]]
+extractTreesTaxidsCSV treesCSVPath = do
+  let myOptions = defaultDecodeOptions {
+         decDelimiter = fromIntegral (ord ',')
+         }
+  inputCSV <- L.readFile treesCSVPath
+  let decodedCsvOutput = V.toList (fromRight (decodeWith myOptions HasHeader inputCSV :: Either String (V.Vector ([Int]))))
+  let treesTaxids = transpose decodedCsvOutput
+  return treesTaxids 
+
