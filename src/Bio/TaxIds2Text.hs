68a396d5cea86f7af80e4757ded9e45da55ed4ed
Florian Eggenhofer <egg@tbi.univie.ac.at>
2015-06-01 14:56:05 +0200

    Added rank commandline parameter to TaxIds2Text

--- src/Bio/TaxIds2Text.hs
+++ src/Bio/TaxIds2Text.hs
@@ -10,15 +10,19 @@ import System.Console.CmdArgs
 import Data.Either.Unwrap
 import qualified Data.ByteString.Char8 as BC
 import Bio.Taxonomy
- 
+import Data.Maybe
+import Text.Read
+
 data Options = Options            
   { taxDumpDirectoryPath :: String,
+    taxonomicRank :: String,
     taxNodeListFilePath :: String
   } deriving (Show,Data,Typeable)
              
 options :: Options
 options = Options
   { taxDumpDirectoryPath = def &= name "i" &= help "Path to input NCBI taxonomy dump files directory",
+    taxonomicRank = "Class" &= name "r" &= help "Requested taxonomic rank - default Class",
     taxNodeListFilePath = def &= name "t" &= help "Path to input taxonomy id list without header"
   } &= summary "TaxIds2Text" &= help "Florian Eggenhofer - 2015" &= verbosity   
         
@@ -26,20 +30,27 @@ main :: IO ()
 main = do
   Options{..} <- cmdArgs options
   graphOutput <- readNamedTaxonomy taxDumpDirectoryPath
-  if isLeft graphOutput then 
-    print ("Could not parse provided taxonomy dump files" ++ show (fromLeft graphOutput))
-    else   
-    do if null taxNodeListFilePath then
-              putStrLn "Provide a path to input taxonomy id list or to RNAlienResult CSV."
-         else 
-         do -- input taxid path present
-            taxidtable <- readFile taxNodeListFilePath
-            let taxidtableentries = map (\l -> read l :: Int) (lines taxidtable)
-            let graph = fromRight graphOutput
-            let maybeParentNodes = map (\taxidtableentry -> getParentbyRank taxidtableentry graph (Just Class)) taxidtableentries
-            let parentNodeStrings = map (\maybeParentNode -> maybe "not found,not found" (\(_,n) ->  printSimpleNode n) maybeParentNode) maybeParentNodes
-            let outputCSV = map (\(txid,parentNodeString) -> (show txid) ++ "," ++ parentNodeString)   (zip taxidtableentries parentNodeStrings)
-            mapM_ putStrLn outputCSV
+  let currentRank = readMaybeRank taxonomicRank 
+  if isNothing currentRank
+    then putStrLn "Please provide a valid taxonomic Rank (e.g. Class)."
+    else do
+      if isLeft graphOutput 
+       then print ("Could not parse provided taxonomy dump files" ++ show (fromLeft graphOutput))
+       else do 
+         if null taxNodeListFilePath 
+           then do 
+             putStrLn "Provide a path to input taxonomy id list or to RNAlienResult CSV."
+           else  do -- input taxid path present
+             taxidtable <- readFile taxNodeListFilePath
+             let taxidtableentries = map (\l -> read l :: Int) (lines taxidtable)
+             let graph = fromRight graphOutput
+             let maybeParentNodes = map (\taxidtableentry -> getParentbyRank taxidtableentry graph (Just Phylum)) taxidtableentries
+             let parentNodeStrings = map (\maybeParentNode -> maybe "not found,not found" (\(_,n) ->  printSimpleNode n) maybeParentNode) maybeParentNodes
+             let outputCSV = map (\(txid,parentNodeString) -> (show txid) ++ "," ++ parentNodeString)   (zip taxidtableentries parentNodeStrings)
+             mapM_ putStrLn outputCSV
 
 printSimpleNode :: SimpleTaxon -> String 
 printSimpleNode snode = show (simpleRank snode) ++ "," ++ BC.unpack (simpleScientificName snode)
+
+readMaybeRank :: String -> Maybe Rank
+readMaybeRank inputString = readMaybe inputString
